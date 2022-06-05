4baff0f2572501e9ef895630d79bbae81baed369
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-06-05 20:49:45 +0530

    feat: setup path alias for components and styles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    config.resolve.plugins.push(new TsconfigPathsPlugin());
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
