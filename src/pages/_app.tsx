0afd8df59054a5ed45b6604dacf3c5047d8b9273
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-06-12 05:06:19 +0530

    WIP: remove code including @apollo/client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { CssBaseline } from "@mui/material";
-import { client } from "@Apollo/client";
-import { ApolloProvider } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
+    <>
       <CssBaseline />
       <Component {...pageProps} />
-    </ApolloProvider>
+    </>
   );
 }
 
