7df6a800a9c2ee95330c8c11c17bbaf7c62d32a7
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-06-12 04:12:30 +0530

    feat: setup firebase in project

---
+++ src/firebase/app.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHPVLDWu_V0OAuUeDrmVW3yATM_XjGVJk",
+  authDomain: "whatsapp-clone-33c55.firebaseapp.com",
+  projectId: "whatsapp-clone-33c55",
+  storageBucket: "whatsapp-clone-33c55.appspot.com",
+  messagingSenderId: "403831689022",
+  appId: "1:403831689022:web:f24c612c9d17b48af5e66f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
