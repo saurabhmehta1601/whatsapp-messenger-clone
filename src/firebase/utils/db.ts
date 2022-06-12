0fbd317fb018fe4a32759e4cef2a45ecbccde5a5
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-06-12 15:02:40 +0530

    feat: add util function to get messages by threadId

--- src/firebase/utils/db.ts
+++ src/firebase/utils/db.ts
@@ -1,5 +1,13 @@
-import { doc, getDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "@Firebase/app";
+import { IMessage } from "chat-app-types";
 
 // returns a promise that resolves to a firebase document using id if not exists returns undefined
 const getFirebaseDoc = async (collectionName: string, docId: string) => {
@@ -14,3 +22,14 @@ export const getThreadByIdFromFirestore = async (id: string) =>
   getFirebaseDoc("thread", id);
 export const getMessageByIdFromFirestore = async (id: string) =>
   getFirebaseDoc("message", id);
+
+export const getMessagesInThread = async (threadId: string) => {
+  const messageCollectionRef = collection(db, "message");
+  const q = query(messageCollectionRef, where("threadId", "==", threadId));
+  const messages: Array<IMessage> = [];
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    messages.push(doc.data() as IMessage);
+  });
+  return messages;
+};
