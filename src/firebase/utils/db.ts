2419112fb40430fc0dc6e15a44b2ecae3512e944
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-06-12 11:06:44 +0530

    refactor: rename firebase db util functions

--- src/firebase/utils/db.ts
+++ src/firebase/utils/db.ts
@@ -1,4 +1,4 @@
-import { doc, DocumentSnapshot, getDoc } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { db } from "@Firebase/app";
 
 // returns a promise that resolves to a firebase document using id if not exists returns undefined
@@ -8,7 +8,9 @@ const getFirebaseDoc = async (collectionName: string, docId: string) => {
   return docSnap.data();
 };
 
-export const getUserByIdFromFirebase = async (id: string) => getFirebaseDoc("user", id);
-export const getThreadByIdFromFirebase  = async (id: string) => getFirebaseDoc("thread", id);
-export const getMessageByIdFromFirebase  = async (id: string) =>
+export const getUserByIdFromFirestore = async (id: string) =>
+  getFirebaseDoc("user", id);
+export const getThreadByIdFromFirestore = async (id: string) =>
+  getFirebaseDoc("thread", id);
+export const getMessageByIdFromFirestore = async (id: string) =>
   getFirebaseDoc("message", id);
