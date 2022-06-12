4849d91dc2cfd92628124bc0062e116a2d669585
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-06-13 01:53:21 +0530

    feat: add custom hook to getActiveUser

---
+++ src/hooks/useActiveUser.tsx
@@ -0,0 +1,19 @@
+import { getUserByIdFromFirestore } from "@Firebase/utils/db";
+import { setActiveUser } from "@Redux/features/activeUser";
+import { useAppDispatch, useAppSelector } from "@Redux/hooks";
+import { IUser } from "chat-app-types";
+import { useEffect, useState } from "react";
+
+export const useActiveUser = () => {
+  const activeUser = useAppSelector((state) => state.activeUser.data);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    (async () => {
+      if (activeUser.id === "") {
+        const user = await getUserByIdFromFirestore("krdrnG9caTRrpQeANlrC");
+        dispatch(setActiveUser(user as IUser));
+      }
+    })();
+  }, []);
+  return activeUser;
+};
