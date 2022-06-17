f6bb6abd246fc9c7a2a2cd7328b9a0e88fe9f45e
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-06-17 14:01:24 +0530

    WIP: phone auth completed

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -6,18 +6,26 @@ import { loginWithFacebook } from "@Firebase/utils/auth";
 import { addUserToFirestore } from "@Firebase/utils/db";
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 import { auth } from "@Firebase/app";
-import PhoneInput, { isValidPhoneNumber } from "react-phone-number-input";
+import { isValidPhoneNumber } from "react-phone-number-input";
+import { setActiveUser } from "@Redux/features/activeUser";
+import { useAppDispatch } from "@Redux/hooks";
+import { useRouter } from "next/router";
 
 const isValidName = (name: string) => {
   return name.length >= 3 && name.length <= 20;
 };
 
 export const LoginForm = () => {
-  const [RC, setRC] = useState<any>(null);
   const [confirmationResult, setConfirmationResult] = useState<any>(null);
   const [phoneNumber, setPhoneNumber] = useState("");
   const [country, setCountry] = useState({ code: "+91", symbol: "IN" });
   const [name, setName] = useState("");
+  const [isOTPFormVisible, setIsOTPFormVisible] = useState(false);
+  const [OTP, setOTP] = useState("");
+  const dispatch = useAppDispatch();
+  const [isVerifyOTPDisabled, setIsVerifyOTPDisabled] = useState(true);
+
+  const router = useRouter();
 
   const handleFacebookLogin = async () => {
     const { user } = await loginWithFacebook();
@@ -33,20 +41,7 @@ export const LoginForm = () => {
   };
 
   const handlePhoneLogin = async () => {
-    const confirmationResult = await signInWithPhoneNumber(
-      auth,
-      country.code + phoneNumber,
-      RC
-    );
-    setConfirmationResult(confirmationResult);
-    console.log("confirmationResult", confirmationResult);
-  };
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-
-  useEffect(() => {
-    const recaptcha = new RecaptchaVerifier(
+    const verifier = new RecaptchaVerifier(
       "sign-in-button",
       {
         size: "invisible",
@@ -59,47 +54,105 @@ export const LoginForm = () => {
       },
       auth
     );
-    setRC(recaptcha);
-  }, []);
+
+    const confirmationResult = await signInWithPhoneNumber(
+      auth,
+      country.code + phoneNumber,
+      verifier
+    );
+    setConfirmationResult(confirmationResult);
+    console.log("confirmationResult", confirmationResult);
+    setIsOTPFormVisible(true);
+  };
+
+  const handleOTPVerification = async () => {
+    try {
+      const result = await confirmationResult.confirm(OTP);
+      console.log("confirmationResult.confirm", result);
+      dispatch(setActiveUser(result.user));
+      router.push("/thread");
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const handleOTPChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOTP(e.target.value);
+
+    if (OTP.length === 6) {
+      setIsVerifyOTPDisabled(false);
+    } else {
+      setIsVerifyOTPDisabled(true);
+    }
+  };
+
   return (
-    <div className={styles.form}>
-      <div className={styles.name}>
-        <PersonIcon />
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={handleNameChange}
-        />
-      </div>
-      <div className={styles.telephone}>
-        <PhoneIcon />
-        <span>{country.code}</span>
-        <input
-          type="number"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
-      </div>
-      <div className={styles.buttonGroup}>
-        <button
-          className={styles.confirm}
-          id="sign-in-button"
-          onClick={handlePhoneLogin}
-          disabled={
-            !(isValidPhoneNumber(phoneNumber, (country as any).symbol) &&
-            isValidName(name))
-          }
-        >
-          CONFIRM
-        </button>
-        <button
-          className={styles.facebookConnect}
-          onClick={handleFacebookLogin}
-        >
-          CONNECT WITH FACEBOOK
-        </button>
-      </div>
-    </div>
+    <>
+      {isOTPFormVisible ? (
+        <div className={styles.form}>
+          <div className={styles.formControl}>
+            <input
+              type="number"
+              placeholder="Enter OTP"
+              value={OTP}
+              onChange={handleOTPChange}
+              max="999999"
+              min="0"
+            />
+          </div>
+          <button
+            onClick={handleOTPVerification}
+            // disabled={isVerifyOTPDisabled}
+          >
+            VERIFY OTP
+          </button>
+        </div>
+      ) : (
+        <div className={styles.form}>
+          <div className={styles.formControl}>
+            <PersonIcon />
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+          <div className={styles.formControl}>
+            <PhoneIcon />
+            <span>{country.code}</span>
+            <input
+              type="number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+          </div>
+          <div className={styles.buttonGroup}>
+            <button
+              className={styles.confirm}
+              id="sign-in-button"
+              onClick={handlePhoneLogin}
+              disabled={
+                !(
+                  isValidPhoneNumber(phoneNumber, (country as any).symbol) &&
+                  isValidName(name)
+                )
+              }
+            >
+              CONFIRM
+            </button>
+            <button
+              className={styles.facebookConnect}
+              onClick={handleFacebookLogin}
+            >
+              CONNECT WITH FACEBOOK
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -12,6 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const db = getFirestore(app);
 export const auth = getAuth(app);
