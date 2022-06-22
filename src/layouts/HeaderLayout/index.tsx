b771dd17bbc96f96206f7d36f5f646263eb1b100
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-06-22 17:13:12 +0530

    fix: merge prop className with component className for HeaderLayout

--- src/layouts/HeaderLayout/index.tsx
+++ src/layouts/HeaderLayout/index.tsx
@@ -2,5 +2,9 @@ import React, { ComponentPropsWithoutRef } from "react";
 import styles from "./styles.module.scss";
 
 export const HeaderLayout = (props: ComponentPropsWithoutRef<"header">) => {
-  return <header className={styles.header}>{props.children}</header>;
+  return (
+    <header className={[styles.header, props.className ?? ""].join(" ")}>
+      {props.children}
+    </header>
+  );
 };
