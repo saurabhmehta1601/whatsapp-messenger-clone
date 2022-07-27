c223a11f26d3a38a93e75d97375bba6d44501da1
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-07-27 16:57:45 +0530

    feat: restrict access to pages based on user auth status

--- src/components/UserProvider.tsx
+++ src/components/UserProvider.tsx
@@ -3,17 +3,19 @@ import { getUserSnapshot } from "@Firebase/utils/db/snapshots";
 import { setActiveUser } from "@Redux/features/activeUser";
 import { useAppDispatch } from "@Redux/hooks";
 import { onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/router";
 import { ReactNode, useEffect, useState } from "react";
 
 export const ActiveUserProvider = (props: { children: ReactNode }) => {
   const dispatch = useAppDispatch();
   const [loadingUser, setLoadingUser] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
     (() => {
       onAuthStateChanged(auth, async (authUser) => {
-        console.log("active user is ", authUser);
         if (authUser) {
+          router.replace("/group");
           getUserSnapshot(authUser.uid, (snap) => {
             if (snap.exists()) {
               const userData = snap.data();
@@ -21,6 +23,7 @@ export const ActiveUserProvider = (props: { children: ReactNode }) => {
             }
           });
         } else {
+          router.replace("/");
           dispatch(setActiveUser(null));
         }
       });
