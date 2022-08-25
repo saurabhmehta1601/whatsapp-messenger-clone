51c35b7b70d02a8daf92baf35ba396332783a475
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-08-25 22:50:42 +0530

    refactor: do not redirect after setting active user in redux state

--- src/components/UserProvider.tsx
+++ src/components/UserProvider.tsx
@@ -3,29 +3,23 @@ import { getUserSnapshot } from "@Firebase/utils/db/snapshots";
 import { setActiveUser } from "@Redux/features/activeUser";
 import { useAppDispatch } from "@Redux/hooks";
 import { onAuthStateChanged } from "firebase/auth";
-import { useRouter } from "next/router";
 import { ReactNode, useEffect, useState } from "react";
 
 export const ActiveUserProvider = (props: { children: ReactNode }) => {
   const dispatch = useAppDispatch();
   const [loadingUser, setLoadingUser] = useState(true);
-  const router = useRouter();
 
   useEffect(() => {
     (() => {
       onAuthStateChanged(auth, async (authUser) => {
         if (authUser) {
-          router.replace("/group");
           getUserSnapshot(authUser.uid, (snap) => {
             if (snap.exists()) {
               const userData = snap.data();
               dispatch(setActiveUser({ ...userData, id: authUser.uid }));
             }
           });
-        } else {
-          router.replace("/");
-          dispatch(setActiveUser(null));
-        }
+        } else { dispatch(setActiveUser(null)); }
       });
       setLoadingUser(false);
     })();
