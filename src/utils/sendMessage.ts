af8996dea63cd26e3defdf81fd811fb596d8d35b
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-08-25 22:39:22 +0530

    refactor: add try catch block to sendMessage

--- src/utils/sendMessage.ts
+++ src/utils/sendMessage.ts
@@ -6,37 +6,40 @@ import {
 import { IMediaMessage, INewChatMessage } from "chat-app-types";
 
 export const sendMessage = async (message: INewChatMessage) => {
-  let createdMessage = null;
+  try {
+    let createdMessage = null;
+    if (message.type == "media") {
+      const downloadURL = await uploadFile({
+        name: message.name,
+        file: message.file,
+      });
 
-  if (message.type == "media") {
-    const downloadURL = await uploadFile({
-      name: message.name,
-      file: message.file,
-    });
+      const newMediaMessage: Omit<IMediaMessage, "id" | "createdAt"> = {
+        type: "media",
+        fileName:
+          message.name.split(".")[0] < 15
+            ? message.name
+            : message.name.substring(0, 15) + "..." + message.extension,
+        mediaURL: downloadURL,
+        sender: message.sender,
+        groupId: message.groupId,
+        extention: message.extension,
+      };
+      createdMessage = await addMessageToFirestore(newMediaMessage as any);
+    }
 
-    const newMediaMessage: Omit<IMediaMessage, "id" | "createdAt"> = {
-      type: "media",
-      fileName:
-        message.name.split(".")[0] < 15
-          ? message.name
-          : message.name.substring(0, 15) + "..." + message.extension,
-      mediaURL: downloadURL,
-      sender: message.sender,
-      groupId: message.groupId,
-      extention: message.extension,
-    };
-    createdMessage = await addMessageToFirestore(newMediaMessage as any);
-  }
+    if (message.type === "text") {
+      createdMessage = await addMessageToFirestore(message);
+    }
 
-  if (message.type === "text") {
-    createdMessage = await addMessageToFirestore(message);
-  }
-
-  //  if message is added to firestore or firebase storage,
-  if (createdMessage) {
-    await updateGroupInFirestore(message.groupId, {
-      lastMessageId: createdMessage.id,
-    });
-    console.log("message added to firebase with id", createdMessage?.id);
+    //  if message is added to firestore or firebase storage,
+    if (createdMessage) {
+      await updateGroupInFirestore(message.groupId, {
+        lastMessageId: createdMessage.id,
+      });
+      console.log("message added to firebase with id", createdMessage?.id);
+    }
+  } catch (error) {
+    console.log("error sending message", error);
   }
 };
