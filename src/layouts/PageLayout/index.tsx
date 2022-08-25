b1b096f7b87231e6d08679aeb2243509ba626486
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-08-25 21:14:48 +0530

    refactor: remove unnecessary layout components with only styles(less markup)

--- src/layouts/PageLayout/index.tsx
+++ src/layouts/PageLayout/index.tsx
@@ -1,17 +1,10 @@
-import { ContentLayout, SidebarLayout } from "layouts/exports";
 import React, { ComponentPropsWithoutRef } from "react";
 import styles from "./styles.module.scss";
 
-interface IProps extends ComponentPropsWithoutRef<"div"> {
-  sidebar: React.ReactNode;
-  content: React.ReactNode;
-}
-
-export const PageLayout = (props: IProps) => {
+export const PageLayout = (props: ComponentPropsWithoutRef<"div">) => {
   return (
     <div {...props} className={[styles.page, props.className ?? ""].join(" ")}>
-      <SidebarLayout style={{ flex: 1 }}>{props.sidebar}</SidebarLayout>
-      <ContentLayout style={{ flex: 2 }}>{props.content}</ContentLayout>
+      {props.children}
     </div>
   );
 };
--- src/layouts/exports.ts
+++ src/layouts/exports.ts
@@ -1,6 +1,4 @@
 export { CreateGroupSidebarLayout } from "./CreateGroupSidebarLayout";
-export { SidebarListLayout } from "./SidebarListLayout";
 export { HeaderLayout } from "./HeaderLayout";
 export { ContentLayout } from "./ContentLayout";
 export { PageLayout } from "./PageLayout";
-export { SidebarLayout } from "./SidebarLayout";
