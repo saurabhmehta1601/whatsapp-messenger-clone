07e89167d1679ebd85ffc6aa34f7437b0981b84b
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-09-13 18:25:06 +0530

    feat: add option to pass image ref to ImageUpload component

--- src/components/ImageUpload/index.tsx
+++ src/components/ImageUpload/index.tsx
@@ -1,61 +1,64 @@
-import React, { ComponentPropsWithoutRef } from "react";
+import React, { ComponentPropsWithRef } from "react";
 import GroupIcon from "@mui/icons-material/Group";
 import styles from "./styles.module.scss";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
 import { useAlert } from "react-alert";
 
-interface IProps extends ComponentPropsWithoutRef<"div"> {
-  handleImageChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+interface IProps extends ComponentPropsWithRef<"div"> {
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const ImageUpload = (props: IProps) => {
-  const [previewSrc, setPreviewSrc] = React.useState("");
-  const alert = useAlert();
+export const ImageUpload = React.forwardRef(
+  (props: IProps, ref: React.Ref<HTMLInputElement>) => {
+    const [previewSrc, setPreviewSrc] = React.useState("");
+    const alert = useAlert();
 
-  const onImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      const file = e.target.files[0];
-      if (file) {
-        if (file.size > 1000000) {
-          alert.error("File size should be less than 1MB");
-          return;
+    const onImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        const file = e.target.files[0];
+        if (file) {
+          if (file.size > 1000000) {
+            alert.error("File size should be less than 1MB");
+            return;
+          }
+          const fileURL = URL.createObjectURL(file);
+          setPreviewSrc(fileURL);
+        } else {
+          setPreviewSrc("");
         }
-        const fileURL = URL.createObjectURL(file);
-        setPreviewSrc(fileURL);
-      } else {
-        setPreviewSrc("");
       }
-    }
-    if (props.handleImageChange) props.handleImageChange(e);
-  };
-  return (
-    <div
-      {...props}
-      className={[styles.imageUploadContainer, props.className].join(" ")}
-    >
-      <label
-        htmlFor="uploadImg"
-        style={previewSrc ? { backgroundImage: `url(${previewSrc})` } : {}}
+      if (props.onChange) props.onChange(e);
+    };
+    return (
+      <div
+        {...props}
+        className={[styles.imageUploadContainer, props.className].join(" ")}
       >
-        {!previewSrc && <GroupIcon className={styles.groupIcon} />}
-        <div className={styles.centerOverlay}>
-          <div className={styles.centeredContent}>
-            <div>
-              <CameraAltIcon fontSize="large" />
+        <label
+          htmlFor="uploadImg"
+          style={previewSrc ? { backgroundImage: `url(${previewSrc})` } : {}}
+        >
+          {!previewSrc && <GroupIcon className={styles.groupIcon} />}
+          <div className={styles.centerOverlay}>
+            <div className={styles.centeredContent}>
+              <div>
+                <CameraAltIcon fontSize="large" />
+              </div>
+              <p>
+                UPLOAD <br /> IMAGE
+              </p>
             </div>
-            <p>
-              UPLOAD <br /> IMAGE
-            </p>
           </div>
-        </div>
-      </label>
-      <input
-        onChange={onImageChange}
-        type="file"
-        id="uploadImg"
-        name="uploadImg"
-        accept="image/png, image/jpeg"
-      />
-    </div>
-  );
-};
+        </label>
+        <input
+          ref={ref}
+          onChange={onImageChange}
+          type="file"
+          id="uploadImg"
+          name="uploadImg"
+          accept="image/png, image/jpeg"
+        />
+      </div>
+    );
+  }
+);
