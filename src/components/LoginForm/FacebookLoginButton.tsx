3f626bccb7cb3f4190cfe24f8deb7115eef840bd
Saurabh <saurabhmehta1601@gmail.com>
2023-04-18 13:24:14 +0530

    update FacebookLoginButton styles

--- src/components/LoginForm/FacebookLoginButton.tsx
+++ src/components/LoginForm/FacebookLoginButton.tsx
@@ -34,11 +34,17 @@ export const FacebookLoginButton = () => {
   };
 
   return (
-    <button
-      className={styles.facebookConnectButton}
-      onClick={handleFacebookLogin}
-    >
-      CONNECT WITH FACEBOOK
-    </button>
+		<div className={styles.btnFB} onClick={handleFacebookLogin}>
+			<div className={styles.FBContent}>
+				<div className={styles.logo}>
+					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" version="1">
+        <path fill="#FFFFFF" d="M32 30a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h28a2 2 0 0 1 2 2v28z"/>
+        <path fill="#4267b2" d="M22 32V20h4l1-5h-5v-2c0-2 1.002-3 3-3h2V5h-4c-3.675 0-6 2.881-6 7v3h-4v5h4v12h5z"/>
+      </svg>
+				</div>
+				<p>Sign in with Facebook</p>
+			</div>
+		</div>
+
   );
 };
--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { FacebookLoginButton } from "./FacebookLoginButton";
 import { PhoneLoginForm } from "./PhoneLoginForm";
+import styles from "./styles.module.scss"
 
 export const LoginForm = () => {
   return (
-    <>
+    <div className={styles.grid}>
       <PhoneLoginForm />
-      <FacebookLoginButton />
-    </>
+      <div className={styles.FBButtonContainer}>
+        <FacebookLoginButton />
+      </div>
+    </div>
   );
 };
